deaa2d3e15f5948f19015d8257ad7cfcdc7238ed
Hugh Lee <u88803494@gmail.com>
2021-10-31 12:08:49 +0800

    Complete to build for deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // region 1. Platform Libraries
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import React, { useEffect } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 // end-region
